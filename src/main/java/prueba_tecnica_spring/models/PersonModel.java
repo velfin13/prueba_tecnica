13387f8841dfb863bb6bbfcb81a51ec4f12815a1
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-07-14 11:00:16 -0500

    validando modelos

--- src/main/java/prueba_tecnica_spring/models/PersonModel.java
+++ src/main/java/prueba_tecnica_spring/models/PersonModel.java
@@ -5,12 +5,17 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,11 +30,27 @@ public class PersonModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@Column(nullable = false)
+	@NotBlank(message = "La name es obligatoria")
+	@NotNull(message = "El campo name es obligatorio")
 	private String name;
+
+	@Column(nullable = false)
+	@NotBlank(message = "La lastname es obligatoria")
+	@NotNull(message = "El lastname name es obligatorio")
 	private String lastname;
+
+	@NotBlank(message = "La identificación es obligatoria")
+	@Pattern(regexp = "\\d{10}", message = "La identificación debe tener 10 dígitos")
+	@Column(nullable = false, unique = true)
 	private String identification;
+
+	@Column(name = "birthdate")
+	@Past(message = "La birthdate debe ser anterior a la fecha actual")
+	@NotNull(message = "birthdate es obligatoria")
 	private Date birth;
-	
+
 	@OneToMany(mappedBy = "person")
 	@JsonIgnore
 	private List<UserModel> users;
--- src/main/java/prueba_tecnica_spring/models/UserModel.java
+++ src/main/java/prueba_tecnica_spring/models/UserModel.java
@@ -1,11 +1,16 @@
 package prueba_tecnica_spring.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,15 +21,30 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name = "users")
 public class UserModel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String username;
-    private String password;
-    private String email;
-    private Boolean session_active;
-    private Boolean status;
-    
-    @ManyToOne
-    private PersonModel person;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "username", nullable = false, unique = true)
+	@NotBlank(message = "La username es obligatoria")
+	@Pattern(regexp = "^[^!@#$%^&*()_+=\\-\\[\\]{};':\"\\\\|,.<>/?]*$", message = "El nombre no debe contener signos")
+	@NotNull(message = "El campo username es obligatorio")
+	private String username;
+
+	@Column(name = "password", nullable = false)
+	@NotNull(message = "El campo password es obligatorio")
+	@NotBlank(message = "La contraseña es obligatoria")
+	@Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
+	@Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+=\\-\\[\\]{};':\"\\\\|,.<>/?]).*$", message = "La contraseña no cumple con los requisitos")
+	private String password;
+
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+
+	private Boolean session_active = false;
+
+	private Boolean status = true;
+
+	@ManyToOne
+	private PersonModel person;
 }
